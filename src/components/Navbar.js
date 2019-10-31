95c4f68c97c0a5d6c567243e0dbe95e41dd75d7e
Ian Baylis <ibaylis@USMDLT824.local>
2019-10-31 17:01:31 -0400

    Added pages and some routing

---
+++ src/components/Navbar.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import logo from '../images/logo.svg';
+import { FaAlignRight } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+class Navbar extends Component {
+    state = {
+        isOpen: false
+    }
+
+    handleToggle = () => {
+        this.setState({ isOpen: !this.state.isOpen })
+    }
+
+    render() {
+        return (
+            <nav className="navbar"> 
+                <div className="nav-center">
+                    <div className="nav-header">
+                        <Link to="/">
+                            <img src={logo} alt="Beach Resort" />
+                        </Link>
+                        <button 
+                            type="button"
+                            className="nav-btn"
+                            onClick={this.handleToggle}
+                        >
+                            <FaAlignRight className="nav-icon" />
+                        </button>
+                    </div>
+                    {/* end of nav header */}
+                    <ul className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/rooms">Rooms</Link>
+                        </li>
+                        <li>
+                            <Link to="/reservations">Reservations</Link>
+                        </li>
+                        <li>
+                            <Link to="/contacts">Contact Us</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Navbar;
