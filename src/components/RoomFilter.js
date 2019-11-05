0c506d91f1c59d769cd66a5691f5053a15e966e8
Ian Baylis <ibaylis@USMDLT824.local>
2019-11-05 16:44:42 -0500

    Added Room Filter components and search functions

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { useContext } from "react";
+import { RoomContext } from "../context";
+import Title from "../components/Title";
 
 export default function RoomFilter() {
+    const context = useContext(RoomContext);
+    const { handleChange, type, capacity, price, minPrice, maxPrice, minSize, maxSize, breakfast, pets } = context;
     return (
-        <div>
-            Hello from Room Filter
-        </div>
+        <section className="filter-container">
+            <Title title="search rooms" />
+            <form className="filter-form">
+                {/* Select Type */}
+                <div className="form-group">
+                    <label htmlFor="type">Room Type</label>
+                    <select 
+                        name="type" 
+                        id="type"
+                        value={type}
+                        className="form-control"
+                        onChange={handleChange} 
+                    />
+                </div>
+                {/* end select type */}
+            </form>
+        </section>
     )
 }
